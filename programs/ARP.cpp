d5c89594f679147e21a4a2040b69f4cd3b6f57d9
Karthick691 <ganeshkarthickm1138.sse@saveetha.com>
2026-02-16 12:43:58 +0530

    Refactor ARP request to use SendARP on Windows
    Updated ARP request implementation for Windows using SendARP.

--- programs/ARP.cpp
+++ programs/ARP.cpp
@@ -1,40 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/if_arp.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
+#include <iphlpapi.h>
 
 int main() {
-    int sock;
-    struct arpreq req;
+    DWORD destIP;
+    ULONG macAddr[2];
+    ULONG macLen = 6;
 
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    destIP = inet_addr("192.168.1.1");
 
-    memset(&req, 0, sizeof(req));
-
-    // Set IP address
-    struct sockaddr_in *sin = (struct sockaddr_in *)&req.arp_pa;
-    sin->sin_family = AF_INET;
-    sin->sin_addr.s_addr = inet_addr("192.168.1.1");
-
-    // Network interface
-    strcpy(req.arp_dev, "eth0");
-
-    // Get ARP entry
-    ioctl(sock, SIOCGARP, &req);
-
-    // Print MAC address
-    printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
-        (unsigned char)req.arp_ha.sa_data[0],
-        (unsigned char)req.arp_ha.sa_data[1],
-        (unsigned char)req.arp_ha.sa_data[2],
-        (unsigned char)req.arp_ha.sa_data[3],
-        (unsigned char)req.arp_ha.sa_data[4],
-        (unsigned char)req.arp_ha.sa_data[5]);
-
-    close(sock);
+    if (SendARP(destIP, 0, macAddr, &macLen) == NO_ERROR) {
+        BYTE *mac = (BYTE*)macAddr;
+        printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+               mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    } else {
+        printf("ARP request failed\n");
+    }
     return 0;
 }
